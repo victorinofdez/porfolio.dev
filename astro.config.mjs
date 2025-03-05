e2c4041ee9285c999ef07623345976244dc04dfb
Victorino Fernández <victorino_fdez@icloud.com>
2025-03-05 20:07:08 +0100

    fist commit porfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  env: {
+    schema: {
+      RESEND_API_KEY: envField.string({ context: "server", access: "secret" }),
+      MY_EMAIL: envField.string({ context: "server", access: "secret" }),
+    },
+  },
+  output: "server",
+  adapter: vercel(),
+});
