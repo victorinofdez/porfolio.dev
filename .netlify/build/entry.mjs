a6fdeea35147869f805755ec154f20fb8a5fe27a
Victorino Fernández <victorino_fdez@icloud.com>
2025-03-05 21:52:18 +0100

    deploy

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,31 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_BRbgv5Nh.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/projects.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["src/pages/projects.astro", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "78b92754-651f-4ce6-b0d0-3122b9e5ea24"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   output: "static",
+  adapter: netlify(),
 });
